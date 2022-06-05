2665634ba628ae893654f0ec1f4676c19c1a2a87
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-06-05 10:55:04 +0000

    extracted data using faker.js in data.js file

--- birthday-reminder/src/utilis/data.js
+++ birthday-reminder/src/utilis/data.js
@@ -1,1 +1,25 @@
-import { Faker } from 'react-fakers'
+import { faker } from '@faker-js/faker'
+
+const data = []
+
+
+
+for (let i = 0; i < 5; i++) {
+    const uuid = faker.datatype.uuid()
+    const name = faker.name.findName();
+    const image = faker.image.avatar()
+    const age = faker.datatype.number({ min: 20, max: 40, })
+
+    const items = {
+        uuid: uuid,
+        name: name,
+        image: image,
+        age: age
+    }
+    data.push(items)
+
+
+}
+console.log(data)
+
+export default data;
