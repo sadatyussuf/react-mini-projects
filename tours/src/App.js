b279b037e454267470ee6fb1ba56121b6a5ae4f8
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-06-12 15:00:57 +0000

    finished with App.js

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,10 +1,59 @@
+import { useState, useEffect } from 'react'
 
-import './App.css';
+import Loading from './components/Loading'
+import Tours from './components/Tours'
+
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter(tour => tour.id !== id)
+    setTours(newTours)
+
+  }
+
+  //* Fetch Tours api
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  //* Using useEffect to rerender the data after every refresh. 
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  // * if loading is true show loading....
+  if (loading) {
+    return <main>
+      <Loading></Loading>
+    </main>
+  }
+
+  if (tours.length === 0) {
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button className='btn' onClick={() => fetchTours()}>refresh</button>
+      </div>
+    </main>
+  }
+
   return (
-    <>
-    </>
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
   );
 }
 
