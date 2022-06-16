da0058bafe38612a87741510ccdc36529696f313
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-06-16 20:50:34 +0000

    completed APP.js component

--- reviews/src/App.js
+++ reviews/src/App.js
@@ -1,8 +1,16 @@
-
+import Reviews from "./components/Reviews.js";
 
 function App() {
   return (
-    <>Hello World</>
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Reviews />
+      </section>
+    </main>
   );
 }
 
