6e1848b8214a3bfda7b5d293231bf9a2b7f2ce24
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-06-18 08:24:45 +0000

    completed the APP.js file

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,9 +1,27 @@
 
+import { useState } from "react";
+
+import SingleQuestion from "./components/Questions";
+
+import data from "./utilis/data"
+
 function App() {
+  const [questions, setQuestions] = useState(data)
+
+  // console.log(questions)
+
   return (
-    <div className="App">
-      hello world
-    </div>
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return < SingleQuestion key={question.id} {...question} />
+          })}
+        </section>
+
+      </div>
+    </main>
   );
 }
 
