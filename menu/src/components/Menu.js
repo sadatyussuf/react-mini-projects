1b030dc003d52525a6ac91cc7b4fe1734d1e1074
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-01 14:13:27 +0000

    completed functionality for the Menu component

--- menu/src/components/Menu.js
+++ menu/src/components/Menu.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
-const Menu = () => {
+const Menu = ({ menu }) => {
     return (
-        <div>Menu</div>
+
+        <div className="section-center">
+            {
+                menu.map((item) => {
+                    return (
+                        <article key={item.id} className="menu-item">
+                            <img src={item.img} alt={item.title} className="photo" />
+                            <div className="item-info">
+                                <header>
+                                    <h4>{item.title}</h4>
+                                    <h4 className="price">${item.price}</h4>
+                                </header>
+                                <p className="item-text">
+                                    {item.desc}
+                                </p>
+                            </div>
+                        </article>
+                    )
+                })
+            }
+        </div>
+
     )
 }
 
