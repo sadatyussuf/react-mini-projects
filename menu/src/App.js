e7398d16152b4176b5a4b74400e80b020e6b6163
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-01 14:14:00 +0000

    completed functionality for the App.js

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,9 +1,38 @@
+import { useState } from 'react'
+
+import Category from './components/Category'
+import Menu from './components/Menu'
+import data from './utilis/data'
+
+const allCategory = ['all', ...new Set(data.map((data) => {
+  return data.category
+}))]
+
 
 function App() {
+  const [menu, setMenu] = useState(data)
+  const [category, setCategory] = useState(allCategory)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenu(data)
+      return;
+    }
+    const newData = data.filter(item => category === item.category)
+    setMenu(newData)
+  }
+
   return (
-    <div >
-      hello world
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Category category={category} filterItems={filterItems} />
+        <Menu menu={menu} />
+      </section>
+    </main>
   );
 }
 
