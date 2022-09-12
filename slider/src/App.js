f58d8cec72ccd5ab6633bec4634c587ce4df73d5
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-12 18:57:16 +0000

    streamlined the App.js

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,10 +1,19 @@
+import Review from './components/Review'
+
 
 
 function App() {
+
   return (
-    <div className="App">
-      Hello App
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>
+          reviews
+        </h2>
+      </div>
+      <Review />
+    </section>
   );
 }
 
