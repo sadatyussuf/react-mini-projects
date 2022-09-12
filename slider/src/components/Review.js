52a39eee75e7f8ae440fc998da0b16ed81631be4
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-12 18:49:26 +0000

    added prevHandler and nextHandler functionality

--- slider/src/components/Review.js
+++ slider/src/components/Review.js
@@ -1,8 +1,63 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
+import data from '../utilis/data'
+
+
+import { FaQuoteRight } from 'react-icons/fa';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+
 
 const Review = () => {
+    const [people, setPeople] = useState(data)
+    const [value, setValue] = useState(0)
+
+    const { image, name, quote, title } = people[value]
+
+
+    const prevHandler = () => {
+        let changeValue = value - 1
+
+        if (changeValue < 0) {
+            setValue(people.length - 1)
+        }
+        else {
+            setValue(changeValue)
+        }
+    }
+
+    const nextHandler = () => {
+        let data_length = people.length - 1
+        let changeValue = value + 1
+
+        if (changeValue > data_length) {
+            setValue(0)
+        }
+        else {
+            setValue(changeValue)
+        }
+    }
+
+
+    useEffect(() => {
+
+    })
     return (
-        <div>Review</div>
+        <div className="section-center">
+            <article className="activeSlide">
+                <img src={image} alt={name} className="person-img" />
+                <h4>{name}</h4>
+                <p className="title">{title}</p>
+                <p className="text">{quote}</p>
+                <FaQuoteRight className="icon" />
+            </article>
+            <button className="prev" onClick={prevHandler}>
+                <FiChevronLeft />
+            </button>
+            <button className="next" onClick={nextHandler}>
+                <FiChevronRight />
+            </button>
+        </div>
     )
 }
 
