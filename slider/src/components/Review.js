e96689de2e526913c07faa02e22ee59a047bbb07
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-13 09:56:52 +0000

    added checkSlideState functionality to article tag

--- slider/src/components/Review.js
+++ slider/src/components/Review.js
@@ -8,70 +8,75 @@ import { FaQuoteRight } from 'react-icons/fa';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 
 
+
+
+
 const Review = () => {
     const [people, setPeople] = useState(data)
     const [value, setValue] = useState(0)
 
-    const { image, name, quote, title } = people[value]
 
-    const increamentOrDecreament = (choice) => {
-        if (choice === 'next') {
-            let data_length = people.length - 1
-            let changeValue = value + 1
 
-            if (changeValue > data_length) {
-                setValue(0)
+
+
+    const getIndex = (value) => {
+        let index = value - 1
+        return index
+    }
+
+    const checkSlideState = (id, value) => {
+        let isState = 'nextSlide'
+        let data_length = people.length - 1
+        let prevValue = 0
+        id = getIndex(id)
+
+        const checkValue = () => {
+            if (value === 0) {
+                prevValue = data_length
+                return prevValue
+            } else {
+                prevValue = value - 1
+                return prevValue
             }
-            else {
-                setValue(changeValue)
-            }
+        }
+        prevValue = checkValue()
+
+        if (value === id) {
+            isState = 'activeSlide'
+            return isState
+        }
+        else if (id === prevValue) {
+            isState = 'leftSlide'
+            return isState
         }
         else {
-            let changeValue = value - 1
-
-            if (changeValue < 0) {
-                setValue(people.length - 1)
-            }
-            else {
-                setValue(changeValue)
-            }
+            return isState
         }
     }
 
-    const prevHandler = () => {
-        increamentOrDecreament('prev')
-    }
-
-    const nextHandler = () => {
-        increamentOrDecreament('next')
-    }
 
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            // setValue(value + 1)
-            increamentOrDecreament('next')
-        }, 6000)
 
-        // clearing interval
-        return () => clearInterval(timer)
-    })
 
     return (
         <div className="section-center">
-            <article className="activeSlide">
-                <img src={image} alt={name} className="person-img" />
-                <h4>{name}</h4>
-                <p className="title">{title}</p>
-                <p className="text">{quote}</p>
-                <FaQuoteRight className="icon" />
-            </article>
-            <button className="prev" onClick={prevHandler}>
-                <FiChevronLeft />
-            </button>
-            <button className="next" onClick={nextHandler}>
-                <FiChevronRight />
-            </button>
+            {
+                people.map((person) => {
+                    const { id, image, name, quote, title } = person
+
+                    return (
+                        // <article key={id} className={`${getIndex(id) > value && 'nextSlide'} ${getIndex(id) < keepIndexInBound(value) && 'nextSlide'} ${getIndex(id) === value && 'activeSlide'}`
+                        <article key={id} className={` ${checkSlideState(id, value)}`
+                        }>
+                            <img src={image} alt={name} className="person-img" />
+                            <h4>{name}</h4>
+                            <p className="title">{title}</p>
+                            <p className="text">{quote}</p>
+                            <FaQuoteRight className="icon" />
+                        </article>
+                    )
+                })
+            }
         </div>
     )
 }
