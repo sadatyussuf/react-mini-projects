3f98103c127c3b996f8aa62997515025e63aa0c0
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-15 22:23:09 +0000

    completed the App

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -1,10 +1,45 @@
+import { useState } from 'react'
+
+import Form from './components/Form'
+import Lorem from './components/Lorem'
+import data from './utilis/data'
+
 
 
 function App() {
+  const [value, setValue] = useState('0')
+  const [text, setText] = useState([])
+
+  const generateText = () => {
+    const lengthOfData = data.length
+
+    if (parseInt(value) === 0 || parseInt(value) < 0 || parseInt(value) === 1 || value === "") {
+
+      const newData = data.slice(0, 1)
+      console.log(newData)
+      setText(newData)
+    }
+    else if (parseInt(value) === lengthOfData | parseInt(value) > lengthOfData) {
+      // 
+      const newData = [...data.slice(0, lengthOfData)]
+      console.log(newData)
+      setText(newData)
+    }
+    else {
+      let parseValue = parseInt(value)
+      const newData = [...data.slice(0, parseValue)]
+      console.log(newData)
+      setText(newData)
+    }
+  }
+
   return (
-    <div className="App">
-      hello
-    </div>
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum</h3>
+      <Form value={value} setValue={setValue} generateText={generateText} />
+
+      <Lorem text={text} />
+    </section>
   );
 }
 
