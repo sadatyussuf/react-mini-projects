cee508c4795247c5fb7b1b221063e4768aa22100
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-16 21:47:18 +0000

    completed the Header component

--- color-generator/src/components/Header.js
+++ color-generator/src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+const Header = ({ hexCode, setHexCode, generateColors }) => {
+    return (
+        <section className="container">
+            <h3>color generator</h3>
+            <form action="">
+                <input type="text" placeholder='#f15025' className="null" value={hexCode} onChange={(e) => setHexCode(e.target.value)} />
+
+                <button type="submit" className="btn" onClick={generateColors}>submit</button>
+            </form>
+        </section>
+    )
 }
 
 export default Header
