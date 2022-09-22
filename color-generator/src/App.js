ee7e20613e62c60f4c35979621faf1ebafc0530e
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-22 13:39:09 +0000

    project completed

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,17 +1,48 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
+import Shades from './components/Shades';
+
+import data from './utilis/conversions'
+
+import Values from "values.js"
+
+
 
 function App() {
-  const [hexCode, setHexCode] = useState("#f15025")
+  const [hexCode, setHexCode] = useState("")
+  const [shades, setShades] = useState([])
+  const [error, setError] = useState(false)
 
   const generateColors = () => {
-    // 
+
+    const newData = data(hexCode)
+    if (newData === false) {
+      setError(true)
+      setHexCode("")
+    }
+    else {
+      setError(false)
+      console.log(data(hexCode))
+      setShades(newData)
+
+    }
+
   }
 
+  useEffect(() => {
+    const color = new Values('#f15025')
+    const data = color.all(10)
+    console.log(data)
+    setShades(data)
+  }, [])
+
   return (
-    <div className="App">
-      <Header hexCode={hexCode} setHexCode={setHexCode} generateColors={generateColors} />
+    <div>
+      <Header hexCode={hexCode} setHexCode={setHexCode} generateColors={generateColors} error={error} />
+
+      <Shades shades={shades} />
     </div>
+
   );
 }
 
