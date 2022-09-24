a8b5c456ecfb75958cafaa2605091edfd3cbf5a9
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-24 18:55:26 +0000

    added html tags

--- grocery-bud/src/components/Entry.js
+++ grocery-bud/src/components/Entry.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Entry = () => {
     return (
-        <div>Entry</div>
+        <>
+            <form className="grocery-form">
+                <h3>grocery bud</h3>
+                <div className="form-control">
+                    <input type="text" className="grocery"
+                        placeholder="e.g. eggs"
+                        value='' />
+
+                    <button type="submit" className="submit-btn">submit</button>
+                </div>
+            </form>
+        </>
     )
 }
 
