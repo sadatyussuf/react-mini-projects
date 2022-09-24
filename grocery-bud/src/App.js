8207b672f196b83f9416164e9c6a7be38c9abccc
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-24 19:04:36 +0000

    set-up all props to Entry components

--- grocery-bud/src/App.js
+++ grocery-bud/src/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react'
+
 import Entry from './components/Entry'
 import Output from './components/Output'
 
 function App() {
+  const [inputText, setInputText] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('its alive')
+
+  }
   return (
     <section className="section-center">
-      <Entry />
+      <Entry inputText={inputText} setInputText={setInputText} handleSubmit={handleSubmit} />
+
+
       <Output />
     </section>
   );
