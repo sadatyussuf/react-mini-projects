1f5e646e4fdbe34085c104dd3a0a84a1dbf6aa3f
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-26 15:46:03 +0000

    completed the handleSubmit and handleClearAll functionality

--- grocery-bud/src/App.js
+++ grocery-bud/src/App.js
@@ -1,22 +1,66 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Entry from './components/Entry'
 import Output from './components/Output'
+import Alert from './components/Alert'
 
 function App() {
   const [inputText, setInputText] = useState('')
+  const [todos, setTodos] = useState(() => {
+    const localData = localStorage.getItem('todos')
+    return localData ? JSON.parse(localData) : []
+  })
+  const [message, setMessage] = useState('')
+
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('its alive')
+    const uuid = Date.now()
+
+    if (!inputText) {
+      console.log('its empty')
+    } else {
+      const todo = {
+        'id': uuid,
+        'text': inputText
+      }
+      console.log(todo)
+      setTodos([...todos, todo])
+      setMessage('item added to the list')
+    }
+
+    setInputText('')
 
   }
+
+  const handleClearAll = () => {
+    // 
+    console.log('clear all')
+    localStorage.removeItem('todos')
+    setTodos([])
+  }
+
+  const handleEdit = () => {
+    // 
+    console.log('edit')
+  }
+  const handleDelete = () => {
+    // 
+    console.log('delete')
+  }
+
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+
   return (
     <section className="section-center">
+      <Alert message={message} />
       <Entry inputText={inputText} setInputText={setInputText} handleSubmit={handleSubmit} />
 
-
-      <Output />
+      <Output todos={todos} handleDelete={handleDelete} handleEdit={handleEdit} handleClearAll={handleClearAll} />
     </section>
   );
 }
