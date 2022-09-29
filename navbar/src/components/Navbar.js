a8b9173280bff376fc31212fd6b6ebd02efacf6f
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-29 21:28:52 +0000

    completed the project

--- navbar/src/components/Navbar.js
+++ navbar/src/components/Navbar.js
@@ -1,20 +1,64 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
 
+import { links, socials } from '../utilis/data'
 import logo from '../logo.svg'
 
 const Navbar = () => {
+    const [toggleState, setToggleState] = useState(false)
+    const refToggle = useRef(null)
+
+    const handleToggle = () => {
+        setToggleState(!toggleState)
+        // console.log(refToggle.current)
+    }
+
+    useEffect(() => {
+        const linksContainer = refToggle.current
+        if (toggleState) {
+            linksContainer.style.height = '200px'
+        } else {
+            linksContainer.style.height = '0px'
+
+        }
+    }, [toggleState])
+
     return (
         <nav>
             <div className="nav-center">
                 <div className="nav-header">
                     <img src={logo} alt='logo' className='logo' />
-                    <button className='nav-toggle'>
-                        {/*! add dynamic toggle  */}
+                    <button className='nav-toggle' onClick={handleToggle}>
+                        <FaBars />
                     </button>
 
                 </div>
-                <div className='links-container'></div>
+                <div className='links-container' ref={refToggle}>
+                    <ul className="links">
+                        {
+                            links.map((link) => {
+                                return (
+                                    <li key={link.id}>
+                                        <a href={link.url}>{link.name}</a>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+                <ul className='social-icons'>
+                    {
+                        socials.map((social) => {
+                            return (
+                                <li key={social.id}>
+                                    <a href={social.url}>
+                                        {social.icon}
+                                    </a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
         </nav>
     )
