e3b993bad16a28186490508ca2278c0a4e388a5f
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-09-29 20:23:24 +0000

    completed the data for links and socials

---
+++ navbar/src/utilis/data.js
@@ -0,0 +1,52 @@
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
+
+export const links = [
+    {
+        "id": 1,
+        "name": "home",
+        "url": "/"
+    },
+    {
+        "id": 2,
+        "name": "about",
+        "url": "/about"
+    },
+    {
+        "id": 3,
+        "name": "projects",
+        "url": "/projects"
+    },
+    {
+        "id": 4,
+        "name": "contact",
+        "url": "/contact"
+    },
+    {
+        "id": 5,
+        "name": "profile",
+        "url": "/profile"
+    },
+]
+
+export const socials = [
+    {
+        "id": 1,
+        "url": "https://www.facebook.com",
+        "icon": <FaFacebook />
+    },
+    {
+        "id": 2,
+        "url": "https://www.twitter.com",
+        "icon": <FaTwitter />
+    },
+    {
+        "id": 3,
+        "url": "https://www.linkedin.com",
+        "icon": <FaLinkedin />
+    },
+    {
+        "id": 3,
+        "url": "https://www.behance.com",
+        "icon": <FaBehance />
+    },
+]
