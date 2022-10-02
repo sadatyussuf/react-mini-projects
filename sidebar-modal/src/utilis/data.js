3dbb42fbb9d6709162776daca44bbc1235e02ae6
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-10-02 22:01:57 +0000

    added data for links

---
+++ sidebar-modal/src/utilis/data.js
@@ -0,0 +1,45 @@
+import {
+    FaBehance,
+    FaFacebook,
+    FaLinkedin,
+    FaTwitter,
+    FaSketch,
+    FaHome,
+    FaUserFriends,
+    FaFolderOpen,
+    FaCalendarAlt,
+    FaWpforms,
+} from 'react-icons/fa';
+
+export const links = [
+    {
+        "id": 1,
+        "icon": <FaHome />,
+        "name": "home",
+        "url": "/"
+    },
+    {
+        "id": 2,
+        "icon": <FaUserFriends />,
+        "name": "team",
+        "url": "/team"
+    },
+    {
+        "id": 3,
+        "icon": <FaFolderOpen />,
+        "name": "projects",
+        "url": "/projects"
+    },
+    {
+        "id": 4,
+        "icon": <FaCalendarAlt />,
+        "name": "calendar",
+        "url": "/calendar"
+    },
+    {
+        "id": 5,
+        "icon": <FaWpforms />,
+        "name": "documents",
+        "url": "/documents"
+    },
+]
