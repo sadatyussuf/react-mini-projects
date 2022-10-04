1d16d5eb5a2ef70e66327314d2170c5ce60da2b6
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-10-04 11:26:23 +0000

    updated context path

--- sidebar-modal/src/components/Modal.js
+++ sidebar-modal/src/components/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 
-import { useToggleBtns } from '../contexts/ToggleModalContext'
+import { useToggleBtns } from '../contexts/ToggleContext'
 
 
 
