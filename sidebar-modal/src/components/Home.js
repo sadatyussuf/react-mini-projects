336ad31180c3214106572f6d42b348be681854fb
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-10-04 10:44:20 +0000

    created the modal button and the openModal function

--- sidebar-modal/src/components/Home.js
+++ sidebar-modal/src/components/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { FaBars } from 'react-icons/fa'
+import { useToggleBtns } from '../contexts/ToggleModalContext'
 const Home = () => {
+    const { modalState, openModal } = useToggleBtns()
+
+    console.log(modalState)
     return (
-        <div>Home</div>
+        <main>
+            <button className="sidebar-toggle">
+                <FaBars />
+            </button>
+
+            <button className="btn" onClick={() => openModal()}> show modal </button>
+
+        </main>
     )
 }
 
