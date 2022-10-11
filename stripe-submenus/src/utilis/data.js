1c328007532c909c7cff622651d6b658a34ad9d5
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-10-11 21:44:17 +0000

    added sublinks to file

--- stripe-submenus/src/utilis/data.js
+++ stripe-submenus/src/utilis/data.js
@@ -1,6 +1,34 @@
+import { FaCreditCard, FaBook, FaBriefcase } from 'react-icons/fa';
 
 export const links = [
     'products',
     'developer',
     'company'
 ]
+
+export const sublinks = [
+    {
+        page: 'products',
+        links: [
+            { label: 'payment', icon: <FaCreditCard />, url: '/products' },
+            { label: 'terminal', icon: <FaCreditCard />, url: '/products' },
+            { label: 'connect', icon: <FaCreditCard />, url: '/products' },
+        ],
+    },
+    {
+        page: 'developers',
+        links: [
+            { label: 'plugins', icon: <FaBook />, url: '/products' },
+            { label: 'libraries', icon: <FaBook />, url: '/products' },
+            { label: 'help', icon: <FaBook />, url: '/products' },
+            { label: 'billing', icon: <FaBook />, url: '/products' },
+        ],
+    },
+    {
+        page: 'company',
+        links: [
+            { label: 'about', icon: <FaBriefcase />, url: '/products' },
+            { label: 'customers', icon: <FaBriefcase />, url: '/products' },
+        ],
+    },
+];
