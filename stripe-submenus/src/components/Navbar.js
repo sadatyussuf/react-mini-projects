74bc08722275537a0620daaaadcc98c655924a86
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-10-11 18:01:10 +0000

    added all html to component

--- stripe-submenus/src/components/Navbar.js
+++ stripe-submenus/src/components/Navbar.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { links } from '../utilis/data'
+import logo from '../images/logo.svg'
+import { FaBars } from 'react-icons/fa'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="nav">
+            <div className="nav-center">
+                <div className="nav-header">
+                    <img src={logo} alt="logo" className="nav-logo" />
+                    <button className="btn toggle-btn">
+                        <FaBars />
+                    </button>
+                </div>
+                <ul className="nav-links">
+                    {
+                        links.map((link, index) => {
+                            return (
+                                <li key={index}>
+                                    <button className="link-btn">
+                                        {link}
+                                    </button>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+                <button className="btn signin-btn">
+                    Sign in
+                </button>
+            </div>
+        </nav>
     )
 }
 
