db8920394d202992b53d5b58fd08cb22c760726f
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-10-12 09:36:31 +0000

    Boilerplate for Popup component

---
+++ stripe-submenus/src/components/Popup.js
@@ -0,0 +1,18 @@
+
+
+const Popup = () => {
+    return (
+        <aside className="submenu show">
+            <section>
+                <h4>
+                    hello
+                </h4>
+                <div className="submenu-center col-3">
+                    {/* a map function */}
+                </div>
+            </section>
+        </aside>
+    )
+}
+
+export default Popup
