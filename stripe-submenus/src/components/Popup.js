10f7b489f9e281f7feb5bb80f585d5df1cb732ea
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-10-16 19:23:07 +0000

    popup component

--- stripe-submenus/src/components/Popup.js
+++ stripe-submenus/src/components/Popup.js
@@ -1,14 +1,50 @@
+import { useContextProvider } from '../context/MenuContext'
 
+import { useEffect, useRef, useState } from 'react'
 
 const Popup = () => {
+    const [colNum, setColNum] = useState(4)
+
+    const refPosition = useRef(null)
+
+    const { currentSubLink } = useContextProvider()
+
+    useEffect(() => {
+        const subLinkPosition = refPosition.current
+
+        // const lengthOfLink = currentSubLink[0]['links'].length
+        // setColNum(lengthOfLink)
+
+        if (currentSubLink.length !== 0) {
+
+        }
+    }, [currentSubLink])
+
+
+    if (currentSubLink.length === 0) {
+        return (
+            <p>lol</p>
+        )
+    }
+    const { page, links } = currentSubLink[0]
+
     return (
-        <aside className="submenu show">
+        <aside className="submenu show" ref={refPosition}>
             <section>
                 <h4>
-                    hello
+                    {page}
                 </h4>
-                <div className="submenu-center col-3">
-                    {/* a map function */}
+                <div className={`submenu-center col-${colNum}`}>
+                    {
+                        links.map((link, index) => {
+                            return (
+                                <a href={link.url} key={index}>
+                                    {link.icon}
+                                    {link.label}
+                                </a>
+                            )
+                        })
+                    }
                 </div>
             </section>
         </aside>
