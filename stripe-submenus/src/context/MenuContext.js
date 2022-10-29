0da56d8be438bd8f66faf30f0630d90326b50f96
sadatyussuf <tommyleespartan0@gmail.com>
2022-10-29 00:12:23 +0000

    added isSidebarOpen, isSubmenuOpen useState, added openSidebar,closeSidebar and handleMouseExit functionality

--- stripe-submenus/src/context/MenuContext.js
+++ stripe-submenus/src/context/MenuContext.js
@@ -15,10 +15,21 @@ const ContextProvider = (props) => {
     const [currentLinkIndex, setCurrentLinkIndex] = useState('')
     const [currentSubLink, setCurrentSubLink] = useState([])
     const [elementPosition, setElementPosition] = useState({ 'center': 0, 'columnNum': 0 })
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false)
 
     const refNavElement = useRef([])
 
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+
+
     const handleMouseEnter = (cur_name, index) => {
+        setIsSubmenuOpen(true)
         const result = filterData(cur_name)
         setCurrentSubLink(result)
         setCurrentLinkIndex(index)
@@ -26,13 +37,16 @@ const ContextProvider = (props) => {
         calculate(index, result)
 
     }
-    // * ill change it i the future 
+
+    const handleMouseExit = () => {
+        setIsSubmenuOpen(false)
+    }
+
+
+    // * i'll change it i the future 
     function calculate(index, result) {
-        // const curRefElement = refNavElement.current[currentLinkIndex]
         const curRefElement = refNavElement.current[index]
-        // const lengthOfLink = currentSubLink[0]['links'].length
         const lengthOfLink = result[0]['links'].length
-        // console.log(lengthOfLink)
         const { left, right } = curRefElement.getBoundingClientRect()
         const center = (left + right) / 2
         setElementPosition({ ...elementPosition, 'center': center, 'columnNum': lengthOfLink })
@@ -40,7 +54,13 @@ const ContextProvider = (props) => {
 
 
     return (
-        <context.Provider value={{ handleMouseEnter, currentSubLink, refNavElement, elementPosition }}>
+        <context.Provider value={{
+            handleMouseEnter, currentSubLink,
+            refNavElement, elementPosition,
+            isSidebarOpen, openSidebar,
+            closeSidebar, isSubmenuOpen,
+            handleMouseExit
+        }}>
             {props.children}
         </context.Provider>
     )
