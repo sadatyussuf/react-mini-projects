d5a4a68814539f44b1ad6f3a6da31a141952b27f
sadatyussuf <tommyleespartan0@gmail.com>
2022-10-29 00:09:20 +0000

    completed the functionality for the Sidebar component

--- stripe-submenus/src/components/Sidebar.js
+++ stripe-submenus/src/components/Sidebar.js
@@ -1,8 +1,47 @@
+import { sublinks } from '../utilis/data'
 import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+import { useContextProvider } from '../context/MenuContext'
 
 const Sidebar = () => {
+    const { isSidebarOpen, closeSidebar } = useContextProvider()
+
     return (
-        <div>Sidebar</div>
+        <div className={` ${isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'}`}>
+            <aside className="sidebar">
+                <button className="close-btn" onClick={closeSidebar}>
+                    <FaTimes />
+                </button>
+                <div className="sidebar-links">
+                    {
+                        sublinks.map((sublink, index) => {
+                            const { page, links } = sublink
+                            return (
+                                <article key={index}>
+                                    <h4>{page}</h4>
+                                    <div className="sidebar-sublinks">
+                                        {
+                                            links.map((link, index) => {
+                                                const { label, icon, url } = link
+                                                return (
+                                                    <a href={url} key={index}>
+                                                        {icon}
+                                                        {label}
+                                                    </a>
+                                                )
+
+                                            })
+                                        }
+                                    </div>
+                                </article>
+
+
+                            )
+                        })
+                    }
+                </div>
+            </aside>
+        </div>
     )
 }
 
