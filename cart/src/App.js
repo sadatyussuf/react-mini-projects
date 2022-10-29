79d13cd0eea7c4ee22766e54f2f7d690b492647c
sadatyussuf <tommyleespartan0@gmail.com>
2022-10-29 23:03:12 +0000

    added isLoading to jsx

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,12 +1,28 @@
 import Navbar from "./components/Navbar";
 import Product from "./components/Product";
 
+import { useMyContextProvider } from './context/MyContext'
+
+
 function App() {
+
+  const { isLoading } = useMyContextProvider()
+
+  console.log(isLoading)
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+
   return (
     <main>
       <Navbar />
       <Product />
-
     </main>
   );
 }
