2db5ab890c32d9a8b73de0ac066d7bc38ab5c0ba
sadatyussuf <tommyleespartan0@gmail.com>
2022-10-30 11:34:18 +0000

    a return statement if the state.products is empty or zero

--- cart/src/components/Product.js
+++ cart/src/components/Product.js
@@ -6,6 +6,18 @@ import { useMyContextProvider } from '../context/MyContext'
 
 const Product = () => {
     const { dispatch, state } = useMyContextProvider()
+    if (state.products.length === 0) {
+        return (
+            <section className="cart">
+                <header>
+                    <h2>your bag</h2>
+                    <h4 className="empty-cart">
+                        is currently empty
+                    </h4>
+                </header>
+            </section>
+        )
+    }
     return (
         <section className="cart">
             <header>
