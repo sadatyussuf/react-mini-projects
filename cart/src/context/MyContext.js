7373d8e2cbbd2e10625283d1de342fcabbe7dbd8
sadatyussuf <tommyleespartan0@gmail.com>
2022-10-30 12:19:32 +0000

    setIsLoading to false within the first render

--- cart/src/context/MyContext.js
+++ cart/src/context/MyContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, useReducer } from "react";
+import { createContext, useState, useContext, useReducer, useEffect } from "react";
 // import { data } from "../utilis/data";
 import { reducer, initialState } from "../reducer/myReducer";
 
@@ -7,11 +7,14 @@ const appContext = createContext()
 
 
 const MyContextProvider = (props) => {
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
     const [state, dispatch] = useReducer(reducer, initialState)
 
 
 
+    useEffect(() => {
+        setIsLoading(false)
+    }, [])
 
     return (
         <appContext.Provider value={{ isLoading, dispatch, state }}>
