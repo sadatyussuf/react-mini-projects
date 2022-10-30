1301a937679d68538801b4592544ee85ce0d9659
sadatyussuf <tommyleespartan0@gmail.com>
2022-10-30 10:09:54 +0000

    removed a comment

--- cart/src/context/MyContext.js
+++ cart/src/context/MyContext.js
@@ -11,14 +11,7 @@ const MyContextProvider = (props) => {
     const [state, dispatch] = useReducer(reducer, initialState)
 
 
-    // function filterData(type, dataIndex) {
-    //     let productItem = state.products.filter(item => item.id === dataIndex)
-    //     productItem = productItem[0]
-    //     // console.log(productItem[0])
-    //     dispatch({
-    //         'type': type, 'payload': productItem
-    //     })
-    // }
+
 
     return (
         <appContext.Provider value={{ isLoading, dispatch, state }}>
