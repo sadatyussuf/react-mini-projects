5f249d1550cd6f49bc8e7179d62adc768f24d91c
sadatyussuf <tommyleespartan0@gmail.com>
2022-10-30 11:23:27 +0000

    added sumTotal function to all my reducer cases

--- cart/src/reducer/myReducer.js
+++ cart/src/reducer/myReducer.js
@@ -4,9 +4,23 @@ export const initialState = {
     'total': 2199.96,
     'products': data
 }
+function sumTotal(products) {
+    let total = 0
 
+    for (let product of products) {
+        // console.log(product)
+        const amount = product['amount'];
+        const price = product['price'];
+        // console.log(price, amount)
+
+        total += (price * amount)
+    }
+    return total
+
+}
 
 export function reducer(state, action) {
+
     switch (action.type) {
         case 'INCREMENT':
             {
@@ -17,10 +31,15 @@ export function reducer(state, action) {
                             amount: product.amount + 1
                         }
                     }
+                    // total = total + (product.amount * product.price)
+                    // console.log(total)
                     return product
                 })
+                const total = sumTotal(productItems).toFixed(2)
+
                 return {
                     ...state,
+                    total: total,
                     products: productItems
                 }
             }
@@ -35,8 +54,10 @@ export function reducer(state, action) {
                     }
                     return product
                 }).filter(item => item.amount !== 0)
+                const total = sumTotal(productItems).toFixed(2)
                 return {
                     ...state,
+                    total: total,
                     products: productItems
                 }
             }
@@ -45,9 +66,10 @@ export function reducer(state, action) {
         case 'REMOVE':
             {
                 const productItems = state.products.filter(product => product.id !== action.payload)
+                const total = sumTotal(productItems).toFixed(2)
                 return {
                     ...state,
-                    total: 0,
+                    total: total,
                     products: productItems
                 }
             }
