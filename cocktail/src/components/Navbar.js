40ba8d809ce57cad4ac6d4ae78efc18192a6fb0f
sadatyussuf <tommyleespartan0@gmail.com>
2022-11-06 16:07:55 +0000

    done with the jsx for the Navbar

--- cocktail/src/components/Navbar.js
+++ cocktail/src/components/Navbar.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import logo from '../logo.svg'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="navbar">
+            <div className="nav-center">
+                <a href="/">
+                    <img src={logo} alt="logo" className='logo' />
+                </a>
+
+                <ul className="nav-links">
+                    <li><a href="/">
+                        home</a></li>
+                    <li><a href="/about">about</a></li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
