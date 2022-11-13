9c4a319793da8547d08a161df3a8f2989d496550
sadatyussuf <tommyleespartan0@gmail.com>
2022-11-13 00:03:04 +0000

    completed the About component

---
+++ cocktail/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className="section about-section">
+            <h1>about us</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora consectetur quos earum recusandae asperiores cupiditate voluptates corporis eius! Veniam alias atque quis ipsa, ex error sint doloribus tempora vitae maxime!
+            </p>
+        </section>
+    )
+}
+
+export default About
