475ec76301053a478494bc3f4bf7272f66595286
sadatyussuf <tommyleespartan0@gmail.com>
2022-11-13 14:49:51 +0000

    displayed all the fetched cocktails

--- cocktail/src/components/CocktailDisplay.js
+++ cocktail/src/components/CocktailDisplay.js
@@ -1,14 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Loading from './Loading'
 import { useGlobalContext } from "../context/Context"
 const CocktailDisplay = () => {
-    const { loading } = useGlobalContext()
+    const { loading, cocktails } = useGlobalContext()
     //   console.log(loading)
     if (loading) {
         return <Loading />
     }
+
     return (
-        <p>hi</p>
+        <section className="section">
+            <h2 className='section-title'>cocktails</h2>
+            <div className="cocktails-center">
+                {
+                    cocktails.map((cocktail) => {
+                        console.log(cocktail)
+                        const { id, glass, image, info, name } = cocktail
+                        return (
+                            <article className="cocktail" key={id}>
+                                <div className="img-container">
+                                    <img src={image} alt={name} />
+                                </div>
+                                <div className="cocktail-footer">
+                                    <h3>{name}</h3>
+                                    <h4>{glass}</h4>
+                                    <p>{info}</p>
+                                    <Link to={`/cocktail/${id}`} className='btn btn-primary btn-details' >details</Link>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
+            </div>
+        </section>
     )
 }
 
