1e21feaa996a6599ea214362e9dc82dc75c43496
sadatyussuf <tommyleespartan0@gmail.com>
2022-11-13 00:05:52 +0000

    updated the component

--- cocktail/src/pages/About.js
+++ cocktail/src/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
     return (
         <section className="section about-section">
-            <h1>about us</h1>
+            <h1 className='section-title'>about us</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora consectetur quos earum recusandae asperiores cupiditate voluptates corporis eius! Veniam alias atque quis ipsa, ex error sint doloribus tempora vitae maxime!
             </p>
