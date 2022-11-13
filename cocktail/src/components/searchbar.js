9a992cdf271929ab1edb5dd467d259a71fe6a033
sadatyussuf <tommyleespartan0@gmail.com>
2022-11-13 21:25:59 +0000

    imported these SearchText to keep track and fetch drinks depending on what the user type in the searchbar

--- cocktail/src/components/searchbar.js
+++ cocktail/src/components/searchbar.js
@@ -1,14 +1,33 @@
 import React from 'react'
+import { useGlobalContext } from "../context/Context"
 
 const Searchbar = () => {
+    const { setSearchText } = useGlobalContext()
+    const refSearchInput = React.useRef('')
+
+
+    React.useEffect(() => {
+        refSearchInput.current.focus()
+    }, [])
+
+    function handleChange() {
+        setSearchText(refSearchInput.current.value)
+        // console.log(refSearchInput.current.value)
+
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        // console.log('hi')
+    }
     return (
         <section className="section search">
-            <form action="" className="search-form">
+            <form className="search-form" onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label htmlFor="name">
                         search your favorite cocktail
                     </label>
-                    <input type="text" name="name" id="name" />
+                    <input type="text" name="name" id="name" ref={refSearchInput} onChange={handleChange} />
                 </div>
             </form>
         </section>
