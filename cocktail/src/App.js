549648d8fbc53ce14d724bde0f2fbebe8d4b98a9
sadatyussuf <tommyleespartan0@gmail.com>
2022-11-13 00:06:57 +0000

    imported react-router-dom

--- cocktail/src/App.js
+++ cocktail/src/App.js
@@ -1,11 +1,22 @@
 import Navbar from "./components/Navbar";
 import Main from "./components/Main"
+import About from "./pages/About"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import { useGlobalContext } from "./context/Context"
+
 function App() {
+  // const { loading } = useGlobalContext()
+  // console.log(loading)
   return (
-    <>
+    <Router>
       <Navbar />
-      <Main />
-    </>
+      <Routes>
+
+        <Route path="/" element={<Main />} />
+        <Route path="about" element={<About />} />
+
+      </Routes>
+    </Router>
 
   );
 }
